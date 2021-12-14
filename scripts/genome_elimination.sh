ec600e8c6716c7800a8631a3c1c9d42f579d73b3
Cenorhabditis <Cenorh@bditis.com>
2021-12-14 13:45:25 +0000

    Update "genome_elimination" script

--- scripts/genome_elimination.sh
+++ scripts/genome_elimination.sh
@@ -4,3 +4,9 @@
 
 # Data recup
 wget -r -nH --user='igfl-UE_NGS_2021' --password='UE_NGS_2021'  ftp://sharegate-igfl.ens-lyon.fr/ -P data/
+
+# Data organisation 
+#   ->  See script "genome_elimination_organisation.sh"
+
+
+
